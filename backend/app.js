a3ce267146c618b2b873d6a1c658c4b3d7032577
Yadier <yadierg7@gmail.com>
2023-09-09 14:36:54 -0400

    first commit

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import fs from "fs";
+const app = express();
+
+const data = JSON.parse(fs.readFileSync("./data.json"));
+
+app.get("/api", (req, res) => {
+  res.send(data);
+});
+
+app.get("/api/:id", (req, res) => {
+  //id is planets name
+  const id = req.params.id;
+  const planet = data.find((el) => el.name === id);
+  res.status(200).json({
+    planet,
+  });
+});
+
+const PORT = 4321;
+app.listen(PORT, () => {
+  console.log(`app listening on port ${PORT}`);
+});
